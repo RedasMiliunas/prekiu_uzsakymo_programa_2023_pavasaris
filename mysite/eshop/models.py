1bfa4cb542bf8038b50baa8dabb2b661b1abafe7
Redas Miliunas <redas.miliunas@outlook.com>
2023-05-05 14:37:14 +0300

    Aprasyti du paprasciausi modeliai (status ir product)

--- mysite/eshop/models.py
+++ mysite/eshop/models.py
@@ -2,4 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Status(models.Model):
-    pass
+    #id stulpelis priskiriamas automatiskai (nereikia kurti, bet reikia zinoti, kad turi ID!)
+    name = models.CharField(verbose_name='Pavadinimas', max_length=50)
+
+class Product(models.Model):
+    name = models.CharField(verbose_name='Pavadinimas', max_length=50)
+    price = models.FloatField(verbose_name='Kaina')
+

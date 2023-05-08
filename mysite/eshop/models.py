17cca2359998d4238227870c76d196ffbd86097a
Redas Miliunas <redas.miliunas@outlook.com>
2023-05-08 12:02:14 +0300

    Sukurtas metodas paskaiciuoti visu uzsakymu eiluciu Bendrai sumai: Order klaseje def total(self); atskirai atvaizduota ir order.html faile

--- mysite/eshop/models.py
+++ mysite/eshop/models.py
@@ -22,6 +22,13 @@ class Order(models.Model):
     date = models.DateTimeField(verbose_name='Data', auto_now_add=True)
     status = models.ForeignKey(to='Status', verbose_name='Busena', on_delete=models.SET_NULL, null=True, blank=True)
 
+    def total(self):
+        total = 0
+        lines = self.lines.all()
+        for line in lines:
+            total += line.suma()
+        return total
+
     def __str__(self):
         return f'{self.customer}, {self.date}, {self.status}'
 

26149c74de77a7ebaae172e9b55e39094daa28bf
Redas Miliunas <redas.miliunas@outlook.com>
2023-05-08 10:53:27 +0300

    Padarytas atskiro produkto aprasymas (kolkas ji pasiekti galima tik rankiniu budu vedant URL adresa: /products/id)

--- mysite/eshop/urls.py
+++ mysite/eshop/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.products, name='products'),
+    path('products/<int:product_id>', views.product, name='product'),
 ]
--- mysite/eshop/views.py
+++ mysite/eshop/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 # Create your views here.
@@ -7,3 +7,9 @@ def products(request):
         'products': Product.objects.all()
     }
     return render(request, 'products.html', context=context)
+
+def product(request, product_id):
+    context = {
+        'product': get_object_or_404(Product, pk=product_id),
+    }
+    return render(request, 'product.html', context=context)

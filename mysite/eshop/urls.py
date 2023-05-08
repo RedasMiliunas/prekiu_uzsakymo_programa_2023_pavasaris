4f8bc9923181ddea32f1240e94c1a6d1b0cbb0c7
Redas Miliunas <redas.miliunas@outlook.com>
2023-05-08 11:35:37 +0300

    Padarytas vieno uzsakymo atvaizdavimas (ne pilnai), paspaudus ant uzsakymo eilutes, nuvedama i vieno uzsakymo dabartini atvaizdavima. orders.html <a href="{% url 'order' element.id/pk %}

--- mysite/eshop/urls.py
+++ mysite/eshop/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.products, name='products'),
     path('products/<int:product_id>', views.product, name='product'),
     path('orders/', views.OrderListView.as_view(), name='orders'),
+    path('orders/<int:pk>', views.OrderDetailView.as_view(), name='order'),
 ]
